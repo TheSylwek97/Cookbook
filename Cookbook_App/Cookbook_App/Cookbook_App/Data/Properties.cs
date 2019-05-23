1e9999d27fa3f7cd335dc43500a709e1d7e83971
TheSylwek97 <sylwia97.28@gmail.com>
2019-05-23 21:34:44 +0200

    Database + some needed elements

---
+++ Cookbook_App/Cookbook_App/Cookbook_App/Data/Properties.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cookbook_App.Data
+{
+    class Properties
+    {
+        //public static IDictionary<string, object> AppProperties { get => Application.Current.Properties; }
+    }
+}
---
+++ Cookbook_App/Cookbook_App/Cookbook_App/Model/ISqliteModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cookbook_App.Model
+{
+    public interface ISqliteModel
+    {
+        int ID { get; set; }
+    }
+}
